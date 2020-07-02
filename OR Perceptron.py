9c06fe40835ef25a69c85122faf7b56a997cc75b
kirtikaj27 <kirtika.jaiswal1996@gmail.com>
2020-07-02 12:36:06 +0530

    Final Version

---
+++ OR Perceptron.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# Define inputs, weights and labels
+weights = [3, 5]
+inputs = [(0,0), (0,1), (1,0), (1,1)]
+labels = [0, 1, 1, 1]
+bias = -2
+outputs = []
+
+# Define Net Sum
+for input, label in zip(inputs, labels):
+	linear_combination = weights[0] * input[0] + weights[1] * input[1] + bias
+	out = int(linear_combination >=0)   # Activation output
+	outputs.append([input[0], input[1], linear_combination, out])
+
+out_frame = pd.DataFrame(outputs , columns=['Input 1', 'Input 2', 'Linear Combination', 'Output'])
+print(out_frame.to_string(index=False))
+
+
+
+
+
